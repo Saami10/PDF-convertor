02f89c6f046e4f02f68fdbc7b2767980753589bc
Saami10 <53862897+Saami10@users.noreply.github.com>
2019-08-07 17:19:43 +0530

    Add files via upload

---
+++ pdfconvertor.py
@@ -0,0 +1,11 @@
+import PyPDF2
+import collections  
+pdf_file=open('ch.pdf','rb')
+read_file=PyPDF2.PdfFileReader(pdf_file)
+numpage=read_file.getNumPages()
+c=collections.Counter(range(numpage))
+for i in c:
+    page=read_file.getPage(i)
+    page_content=page.extractText()
+    page_content=page_content.replace("\n","")
+    print(page_content.encode('utf-8'))
