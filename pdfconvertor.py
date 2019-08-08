ee9f77c2fd705c86b7a31aa88597f19add02138a
Saami10 <53862897+Saami10@users.noreply.github.com>
2019-08-08 11:46:48 +0530

    Update pdfconvertor.py

--- pdfconvertor.py
+++ pdfconvertor.py
@@ -1,10 +1,10 @@
 import PyPDF2
-import collections  
+#import collections  
 pdf_file=open('ch.pdf','rb')
 read_file=PyPDF2.PdfFileReader(pdf_file)
 numpage=read_file.getNumPages()
-c=collections.Counter(range(numpage))
-for i in c:
+#c=collections.Counter(range(numpage))
+for i in range(numpage):
     page=read_file.getPage(i)
     page_content=page.extractText()
     page_content=page_content.replace("\n","")
